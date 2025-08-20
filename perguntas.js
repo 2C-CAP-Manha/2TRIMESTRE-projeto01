f18ca6a424f97fc9d3b332c9b92bc76297267a34
isa&ana&luan <i.melo@escola.pr.gov.br>
2025-08-20 12:46:07 +0000

    salvar

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+
+
+criaCartao(
+'B99',
+'Quem é o Capitão novo do departamento de policia?',
+'Capitão Raymond Holt'
+
+)
+criaCartao(
+'B99',
+'Qual o "protagonista" da série?',
+'Detetive Jake Peralta'
+
+)
+criaCartao(
+'B99',
+'Qual o nome da paixonite de Jake Peralta?',
+'Detetive Amy Santiago'
+
+)
+criaCartao(
+'B99',
+'Quem é o Sargento da B99?',
+'Sargento Terry Jeffords'
+)
